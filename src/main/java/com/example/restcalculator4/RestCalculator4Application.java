6b21f6c37b21c7330d9db8207addf1429760b866
Tobias Jahodinsky <wi21b009@technikum-wien.at>
2022-05-19 11:37:10 +0200

    Initial commit

---
+++ src/main/java/com/example/restcalculator4/RestCalculator4Application.java
@@ -0,0 +1,13 @@
+package com.example.restcalculator4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCalculator4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCalculator4Application.class, args);
+    }
+
+}
